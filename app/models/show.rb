952cc447b5ede5ff8b9793cb0d4565a2a0d319bf
Trevor Low <tlow94@uw.edu>
2020-05-02 21:07:32 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -2,6 +2,7 @@ class Show < ActiveRecord::Base
   def self.highest_rating
     self.maximum('rating')
   end
-  def most_popular_show
+  def self.most_popular_show
+    highest_rating.find(1)
   end
 end
